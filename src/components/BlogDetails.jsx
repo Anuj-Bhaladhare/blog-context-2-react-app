d48237130f3f355f3076a88761a148d70aef9812
Anuj-Bhaladhare <125978464+Anuj-Bhaladhare@users.noreply.github.com>
2023-08-07 11:28:48 +0530

    define new API key

---
+++ src/components/BlogDetails.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const BlogDetails = ({post}) => {
+    return (
+        <div>
+            <NavLink to={`/blog/${post.id}`}>
+                <p>{post.title}</p>
+            </NavLink>
+            <div>
+                <p>
+                {`By ${post.author} on ${<NavLink to={`/categories/${post.category.replaceAll(" ", "-")}`}><p>{post.category}</p></NavLink>}`}  
+                </p>
+                <p>Posted ov {post.date}</p>  
+                <p>{post.contant}</p>
+                <div>
+                    {
+                        post.tags.map( (tag, index) => {
+                            return (
+                                <NavLink key={index} to={`/tags/${tag.replaceAll(" ", "-")}`}>
+                                    <p>#{tag}</p>
+                                </NavLink>
+                            )
+                        })
+                    }
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default BlogDetails;
--- src/pages/BlogsPage.jsx
+++ src/pages/BlogsPage.jsx
@@ -1,6 +1,9 @@
-import React, { useContext, useState } from "react";
-import { useLocation, useNavigation } from "react-router-dom";
+import React, { useContext, useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 import { AppContext } from "../context/AppContext";
+import { baseUrl } from "../baseUrl";
+import Header from "../components/Header";
+import BlogDetails from "../components/BlogDetails.jsx";
 
 const BlogsPage = () => {
 
@@ -8,17 +11,62 @@ const BlogsPage = () => {
     const[relatedBlog, setReletadeBlog] = useState();
     const[loading, setLoading] = useContext(AppContext);
     const location = useLocation();
-    const navigation = useNavigation();
+    const navigation = useNavigate();
     const blogID = location.pathname.split("/").at( -1 );
 
     const fetchBlogLocation = async() => {
-        
+        let url = `${baseUrl}?blogId=${blogID}`;
+        setLoading(true);
+        try{
+           const responce = await fetch(url);
+           const data = await responce.json();
+           
+           setBlog(data.blog);
+           setReletadeBlog(data.relatedBlogs);
+        }
+        catch(error){
+            console.log(error);
+            setBlog(null);
+            setReletadeBlog([]);
+        }
+        setLoading(false);
     }
+
+    useEffect( () => {
+        if(blogID) {
+            fetchBlogLocation();
+        }
+    },[location.pathname]);
 
     return (
 
         <div>
-            BlogsPage
+            <Header />
+            <div>
+                <button onClick={() => navigation( -1 ) }>Back</button>
+                <div>
+                    {
+                        loading ? (
+                            <div>Loading</div>
+                        ) :
+                        (
+                            blog ? (
+                                <div>
+                                    <BlogDetails post={blog}/>
+                                    <h2>Related Blog</h2>
+                                    {
+                                        relatedBlog.map( (post) => <div>
+                                            <BlogDetails post={post}/>
+                                        </div>)
+                                    }
+                                </div>
+                            ) : (
+                                <div>No Blog Available</div>
+                            )
+                        )
+                    }
+                </div>
+            </div>
         </div>
     )
 }
